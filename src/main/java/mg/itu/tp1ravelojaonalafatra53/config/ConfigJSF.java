4de0322f68e094773f2a07afc2fc6bbe73403466
LafatraFitia <81089666+LafatraFitia@users.noreply.github.com>
2024-03-24 22:21:29 +0300

    Ajout de la classe de configuration JSF config.ConfigJSF

---
+++ src/main/java/mg/itu/tp1ravelojaonalafatra53/config/ConfigJSF.java
@@ -0,0 +1,10 @@
+package mg.itu.tp1ravelojaonalafatra53.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+
+}
