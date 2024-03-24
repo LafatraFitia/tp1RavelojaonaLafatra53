1530c402b1deb8c85bfdc54ac79b194b3a699eab
LafatraFitia <81089666+LafatraFitia@users.noreply.github.com>
2024-03-24 22:23:45 +0300

    ajout javadoc config.ConfigJSF

--- src/main/java/mg/itu/tp1ravelojaonalafatra53/config/ConfigJSF.java
+++ src/main/java/mg/itu/tp1ravelojaonalafatra53/config/ConfigJSF.java
@@ -3,6 +3,10 @@ package mg.itu.tp1ravelojaonalafatra53.config;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.faces.annotation.FacesConfig;
 
+/**
+ * @author Lafatra
+ * Classe de configuration de JSF
+ */
 @ApplicationScoped
 @FacesConfig
 public class ConfigJSF {
